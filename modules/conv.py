451047b1d912457800acbac5829c70a1de58eb2b
minzwon <minz@suno.ai>
2024-02-13 10:16:22 +0000

    update FMA model

--- modules/conv.py
+++ modules/conv.py
@@ -2,17 +2,17 @@
 #
 # Copyright 2023 ByteDance Inc.
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), 
-# to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”),
+# to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
 # and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 #
 # The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 #
-# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
- 
+
 from torch import nn
 from einops import rearrange
 
@@ -52,16 +52,11 @@ class Conv2dSubsampling(nn.Module):
         strides (list): Sizes of strides.
         n_bands (int): Number of frequency bands.
     """
-    def __init__(self, 
-                 idim, 
-                 hdim,
-                 odim, 
-                 strides=[2, 2],
-                 n_bands=64
-                 ):
+
+    def __init__(self, idim, hdim, odim, strides=[2, 2], n_bands=64):
         """Construct an Conv2dSubsampling object."""
         super(Conv2dSubsampling, self).__init__()
-        
+
         self.conv = nn.Sequential(
             Res2dModule(idim, hdim, (2, strides[0])),
             Res2dModule(hdim, hdim, (2, strides[1])),
